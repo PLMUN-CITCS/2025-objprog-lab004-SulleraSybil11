165be499d15ca36d70d8da1abb5f92e292662078
SulleraSybil11 <sullerasybil_bscs@plmun.edu.ph>
2025-02-09 19:00:46 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 03

---
+++ ArithmeticDemo.java
@@ -0,0 +1,24 @@
+public class ArithmeticDemo {
+    public static void main(String[] args) {  //Defining main method
+
+
+int num1 = 10;  //Declaring int variable
+int num2 = 5;  //Declaring int variable
+double num3 = 3.0;  //Declaring double variable
+int sum = num1 + num2;  //Declaring int variables
+int difference = num1 - num2;  //Declaring int variables
+int product = num1 * num2;  //Declaring int variables
+int quotient = num1 / num2;  //Declaring int variables
+int remainder = num1 % num2;  //Declaring int variables
+int expressionResult = num1 + num2 * 2 - (num1 / num2);  //Declaring int variables
+
+System.out.println("Sum: " + sum);  //Printing all out the variables
+System.out.println("Difference: " + difference);
+System.out.println("Product: " + product);
+System.out.println("Quotient: " + quotient);
+System.out.println("Remainder: " + remainder);
+System.out.println("Expression Result: " + expressionResult); // Explain order of operations
+
+
+    }
+}
